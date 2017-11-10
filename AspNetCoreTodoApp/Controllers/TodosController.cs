1ec0285ab775c67438027ee12b43e09d69f0eefd
Yury Kozhenov <yurykozhenov@gmail.com>
2017-11-11 00:20:54 +0200

    Add todos CRUD

---
+++ AspNetCoreTodoApp/Controllers/TodosController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AspNetCoreTodoApp.Models;
+
+namespace AspNetCoreTodoApp.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class TodosController : Controller
+    {
+        private readonly TodoContext _context;
+
+        public TodosController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.Todos.Count() == 0)
+            {
+                _context.Todos.Add(new Todo { Name = "Todo1" });
+                _context.SaveChanges();
+            }
+        }
+
+        // GET: api/todos
+        [HttpGet]
+        public IEnumerable<Todo> GetTodos()
+        {
+            return _context.Todos;
+        }
+
+        // GET: api/todos/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTodo([FromRoute] long id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var todo = await _context.Todos.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(todo);
+        }
+
+        // PUT: api/todos/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodo([FromRoute] long id, [FromBody] Todo todo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != todo.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(todo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok(todo);
+        }
+
+        // POST: api/todos
+        [HttpPost]
+        public async Task<IActionResult> PostTodo([FromBody] Todo todo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Todos.Add(todo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTodo", new { id = todo.Id }, todo);
+        }
+
+        // DELETE: api/todos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodo([FromRoute] long id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var todo = await _context.Todos.SingleOrDefaultAsync(m => m.Id == id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.Todos.Remove(todo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TodoExists(long id)
+        {
+            return _context.Todos.Any(e => e.Id == id);
+        }
+    }
+}
--- AspNetCoreTodoApp/Startup.cs
+++ AspNetCoreTodoApp/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using AspNetCoreTodoApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AspNetCoreTodoApp
 {
@@ -23,6 +25,7 @@ namespace AspNetCoreTodoApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("todo_app"));
             services.AddMvc();
         }
 
