283df99919005bee82525349e8f1f0364ab0d0fc
Yury Kozhenov <yurykozhenov@gmail.com>
2017-11-11 00:50:25 +0200

    Add Swagger

--- AspNetCoreTodoApp/Startup.cs
+++ AspNetCoreTodoApp/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using AspNetCoreTodoApp.Models;
 using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace AspNetCoreTodoApp
 {
@@ -27,6 +28,12 @@ namespace AspNetCoreTodoApp
         {
             services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("todo_app"));
             services.AddMvc();
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +43,15 @@ namespace AspNetCoreTodoApp
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseMvc();
         }
